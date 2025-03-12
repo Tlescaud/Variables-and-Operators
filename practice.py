e5a5bd5e49f52abce1b12a6cd94d1734612ab082
Tlescaud <lescaud91@gmail.com>
2025-03-12 13:28:09 -0400

    Initial commit

---
+++ practice.py
@@ -0,0 +1,49 @@
+# declaring variables
+
+x = "tom"
+print (x)
+
+if (x == "tom"):
+    print("Hi tom")
+
+print('\n')
+
+
+# using variables for conditional logical statements with built-in functions "if",  "else"
+
+y = "mike"
+print (y)
+if (y == "mike"):
+    print("What the heck is up Mike")
+else:
+    print("this is certainly not you!!")
+
+print('\n')
+
+#comparison operators
+
+jelly = 100
+peanut_butter = 100
+
+if jelly == peanut_butter:
+    print("You have everything to make a sandwich!!")
+else:
+    print("You can not make a sandwich!!")
+
+print('\n')
+
+# Asking for user input in the terminal
+
+user_input = input(" Please enter something: ")
+print("You entered:", user_input)
+
+# Changing the input variable with continued code
+
+# THE IMPORTANT ASPECT HERE IS THE INPUT FUNCTION
+
+# USING SOME SORT OF VARIABLE
+
+# PARATHESIS, DOUBLE COLUMN ALL IN CORRECT PLACES
+
+x = input("write your initials: ")
+print("Your initials are:" , x )
---
+++ python.py
@@ -0,0 +1,30 @@
+# writing simple programs using variables. (First getting started)
+# day 1
+# lets goooooooooo!!!!
+
+
+print("\n") # using this to create space within my terminal
+
+hello ="yes"
+print (hello)
+
+print("\n")
+
+
+
+
+
+hello_there ="loving this program journey"
+print (hello_there)
+
+
+
+
+
+print("\n")
+
+message ="using variables project completed"
+print(message)
+
+print("\n")
+
